0cfa1e85bc7b13424dbeb167dbabe1a9387993e5
Solring Lin <sherry12714@gmail.com>
2021-11-05 17:29:54 +0000

    add vim, iTerm2, Firefox.

--- setup.sh
+++ setup.sh
@@ -2,9 +2,13 @@
 brew install git
 brew install python@3.10
 brew install node
+brew install vim
 
 # docker desktop
 brew install --cask docker
 
 # VSCode IDE
 brew install --cask visual-studio-code
+
+brew install --cask iterm2
+brew install --cask firefox
